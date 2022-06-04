1dee07e70882b39293093fa324f005ca3c6e3bbf
Ervin <evgnz24@gmail.com>
2022-06-03 23:01:43 -0500

    Started over with the JS logic

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+  // the constructor...
+  this.title;
+  this.author;
+  this.pages;
+  this.read;
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
