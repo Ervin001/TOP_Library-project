650e4fa551e5bd7772fe59ce3044fc080e625050
Ervin <evgnz24@gmail.com>
2022-08-24 17:29:55 -0500

    completed

--- script.js
+++ script.js
@@ -36,12 +36,12 @@ class Library {
 
     const containerEl = document.querySelector('.library-container');
     const card = `
-        <div class="card">
+        <div class="card" ">${title}
         <svg style="width: 40px; height: 40px" viewBox="0 0 24 24">
          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
       <path d="M11.25 4.533A9.707 9.707 0 006 3a9.735 9.735 0 00-3.25.555.75.75 0 00-.5.707v14.25a.75.75 0 001 .707A8.237 8.237 0 016 18.75c1.995 0 3.823.707 5.25 1.886V4.533zM12.75 20.636A8.214 8.214 0 0118 18.75c.966 0 1.89.166 2.75.47a.75.75 0 001-.708V4.262a.75.75 0 00-.5-.707A9.735 9.735 0 0018 3a9.707 9.707 0 00-5.25 1.533v16.103z" />
     </svg>
-         <div class="title">${title}</div>
+         <div class="title</div>
          <div class="name-of-author info">${author}</div>
          <div class="number-of-pages info">${pages}</div>
          <div class="status info">${read}</div>
@@ -56,6 +56,8 @@ class Library {
          </div>
          `;
     containerEl.insertAdjacentHTML('beforeend', card);
+    this.#dataCard();
+    console.log(this.#booksArray);
   }
 
   #clearInputs() {
@@ -69,15 +71,31 @@ class Library {
     readRadio.checked = false;
   }
 
-  #deleteBookUI(index) {
+  #dataCard() {
+    const cardsEl = document.querySelectorAll('.card');
+
+    cardsEl.forEach((card, i) => {
+      card.dataset.card = `${i}`;
+    });
+  }
+
+  #deleteBookUI() {
     const containerEl = document.querySelector('.library-container');
 
     containerEl.addEventListener('click', (e) => {
-      // if (e.target.className === 'trash') console.log(e);
-      const target = e.target;
-      if (target.matches('svg')) console.log(e);
-      // e.stopPropagation();
+      if (e.target.closest('.trash') !== null) {
+        // Remove card from DOM
+        e.target.closest('.card').remove();
+        this.#deleteBook(+e.target.closest('.card').dataset.card);
+        console.log(this.#booksArray);
+        this.#dataCard();
+        console.log(this.#booksArray);
+      }
     });
+  }
+
+  #deleteBook(index) {
+    return this.#booksArray.splice(index, 1);
   }
 
   start() {
